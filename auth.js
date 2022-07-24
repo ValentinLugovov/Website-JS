83d10e0ed2967370bfddbcf4b3ea71e8ea55ccbf
Evan_Brooks <valentinlugovov@gmail.com>
2022-07-24 07:04:03 +0300

    Adding a library for downloading PDF.  Refactoring of some sections. Adding footer.

--- auth.js
+++ auth.js
@@ -1,16 +1,73 @@
 /*=====================Authorization=========================*/
 
 let obj = {
-  55183: "Анна Филиппова",
-  12: "Жопа с пальцем",
-  23: "Палец в Жопе",
-  32: "Корень Имбиря",
+  55183: ["Филиппова А.Ю", 123],
+  12: ["Жопа с пальцем", 1],
+  23: ["Палец в Жопе", 2],
+  32: ["Корень Имбиря", 3],
 };
 
-// let loginButton = document.querySelector(".loginButton");
+let loginButton = document.querySelector("#loginButton");
+let authWindow = document.querySelector("#myPrompt");
+let tabNum = document.querySelector("#num1");
+let password = document.querySelector("#num2");
+let okbtn = document.querySelector("#okbtn");
+let error = document.querySelector("#error");
+let name = document.querySelector("#name");
+let tabName = document.querySelector("#tabName");
+let logoutButton = document.querySelector("#logoutButton");
 
-// loginButton.addEventListener("click", function () {
-//   let
-// });
+loginButton.addEventListener("click", () => {
+  authWindow.style.visibility = "visible";
+});
 
-document.getElementById("myPrompt").style.display = "block";
+okbtn.addEventListener("click", () => findLogin());
+
+function findLogin() {
+  for (let key in obj) {
+    if (key == tabNum.value) {
+      if (obj[key][1] == password.value) {
+        name.textContent = obj[key][0];
+        tabName.textContent = obj[key][1];
+        tabNum.value = "";
+        password.value = "";
+        error.style.visibility = "hidden";
+        loginButton.style.display = "none";
+        logoutButton.style.display = "block";
+        return (authWindow.style.visibility = "hidden");
+      }
+    } else {
+      error.style.visibility = "visible";
+    }
+  }
+}
+
+logoutButton.addEventListener("click", () => {
+  location.reload();
+});
+
+/*=====================PrintPDF=========================*/
+
+function generatePDF() {
+  const element = document.getElementById("sterilityAssessment");
+  var opt = {
+    margin: 0,
+    filename: "myfile.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "a4", orientation: "landscape" },
+    //  pagebreak: { mode: ["css", "legacy"] },
+  };
+  html2pdf().set(opt).from(element).save();
+}
+
+let download = document.querySelector("#download");
+
+download.addEventListener("click", () => generatePDF());
+
+/*=====================Archive=========================*/
+
+let archive = {};
+
+// Сделать отдельный объект и туда сохранять pdf/jpeg/html?
+// Нужно ли потом редактировать те файлы?
--- script.js
+++ script.js
@@ -5,13 +5,29 @@
 let protocols = document.querySelector("select");
 let options = protocols.querySelectorAll("option");
 let btn = document.querySelector("#btn");
-let div = document.querySelector(".show");
+let val1 = document.querySelector("#val1");
+let main = document.querySelector(".main");
+let sterilityAssessment = document.querySelector("#sterilityAssessment");
 
-// protocols.addEventListener("change", function () {
-//   console.log(this.value);
-//   // div.classList.add('show')
-//   table1();
-// });
+// Клонирование таблицы и изменение ее id
+let table = sterilityAssessment.cloneNode(true);
+table.id = "table123";
+main.appendChild(table);
+let d = document.querySelector("#table123");
+
+protocols.addEventListener("change", function () {
+  if (protocols.value == "val1") {
+    sterilityAssessment.style.display = "block";
+  } else {
+    sterilityAssessment.style.display = "none";
+  }
+  if (protocols.value == "val2") {
+    d.style.backgroundColor = "red";
+    d.style.display = "block";
+  } else {
+    d.style.display = "none";
+  }
+});
 
 /*=====================SerialNumber=========================*/
 
@@ -40,6 +56,10 @@ let expirationDate = document.querySelectorAll(".expirationDate");
 let data = new Date();
 
 for (let date of expirationDate) {
+  dateFormat(date);
+}
+
+function dateFormat(date) {
   let month = data.getMonth() + 1;
   let day = data.getDate();
 
@@ -49,6 +69,7 @@ for (let date of expirationDate) {
   if (day < 10) {
     day = "0" + day;
   }
+
   date.textContent = day + "." + month + "." + data.getFullYear();
 }
 
@@ -56,16 +77,7 @@ for (let date of expirationDate) {
 
 let verificationDate = document.querySelector(".verificationDate");
 
-let month = data.getMonth() + 1;
-let day = data.getDate();
-
-if (month < 10) {
-  month = "0" + month;
-}
-if (day < 10) {
-  day = "0" + day;
-}
-verificationDate.textContent = day + "." + month + "." + data.getFullYear();
+dateFormat(verificationDate);
 
 /*=====================SampleNumber=========================*/
 
@@ -142,8 +154,28 @@ sowingDate.addEventListener("click", function func() {
   });
 });
 
+// sowingDate.addEventListener("click", (event) => changeData(sowingDate, event));
+
+// function changeData(elem, event) {
+//   let input = document.createElement("input");
+//   input.value = elem.textContent;
+//   elem.innerHTML = "";
+//   console.log("why");
+//   elem.append(input);
+//   elem.removeEventListener("click", changeData);
+
+//   console.log(event.target.className, elem.textContent);
+
+//   input.addEventListener("blur", () => {
+//     elem.innerHTML = input.value;
+//     input.remove();
+//     elem.addEventListener("click", changeData);
+//     console.log(2);
+//   });
+// }
+
 /*=====================ResultDate=========================*/
 
 let resultDate = document.querySelector(".resultDate");
 
-resultDate.textContent = day + "." + month + "." + data.getFullYear();
+dateFormat(resultDate);
